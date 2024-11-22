e1aaf728ce312d8d52ec1236633f2c0b96b38d84
Jerónimo Vicente <jerovicvid@gmail.com>
2024-11-22 18:04:57 +0100

    commit

--- Practicas/Practica 8/Ejercicio 5/funciones1.py
+++ Practicas/Practica 8/Ejercicio 5/funciones1.py
@@ -1,16 +1,65 @@
 ''' By Jerónimo Vicente '''
 
 def mayor(x, y):
+    """
+     Compara dos números y muestra el mayor de ellos.
+
+     Parámetros:
+     x (int o float): Primer número a comparar.
+     y (int o float): Segundo número a comparar.
+
+     Devuelve:
+     None
+     """
     print(f"El número mayor entre {x} y {y} es: {max(x, y)}.")
 
 mayor(6,90)
 mayor(x=45, y=89)
 
 def area(altura, base=8):
+    """
+    Calcula el área de un rectángulo dado su altura y base.
+
+    Parámetros:
+    altura (int o float): Altura del rectángulo.
+    base (int o float, opcional): Base del rectángulo, por defecto es 8.
+
+    Devuelve:
+    int o float: El área del rectángulo.
+        """
     return base * altura
 
 print(area(7, 3))
 print(area(altura=5))
 
 def suma(*numeros):
-    return sum(numeros)
+    return sum(*numeros)
+
+valores = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+print(suma(valores))
+
+def peque(**diccionario):
+    return min(diccionario, key=diccionario.get)
+
+habitaciones = {"salón": 20, "terraza": 10, "lavadero": 5, "despacho": 7, "cuarto": 15}
+
+print(peque(**habitaciones))
+
+def repetir(texto, /,*, entero):
+    for _ in range(entero): # _ es se usa por convención para variables que no se van a usar.
+        print(texto)
+
+repetir("repito", entero=6)
+
+def funcion(func1, func2):
+    func1()
+    func2()
+
+asignaturas = {"Acceso a Datos": 5, "Programación": 6, "Sistemas": 8, "Interfaces": 3}
+funcion(lambda: mayor(23, 15), lambda: print(peque(**asignaturas)))
+
+# El uso de lambda es para poder pasar funciones como argumentos.
+
+help(mayor)
+help(area)
